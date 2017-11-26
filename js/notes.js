e30bce9a94956df31d619db6ddab4fc5b0169ddd
Rosa Santiago <rosa.d.santiago85@gmail.com>
2017-11-26 13:12:41 -0600

    implement delete a note, and create a new  note and add searching filters

--- js/notes.js
+++ js/notes.js
@@ -1,10 +1,82 @@
 var app = new Vue({
   el: '#app',
   data: {
-    notes: [
+    notes: [],
+    selectedNote: {},
+    searchNoteText: ""
+  },
+  mounted: function() {
+    this.notes = [
       {id: 1, body: "This is a first test", timestamp: Date.now()},
       {id: 2, body: "This is a second test", timestamp: Date.now()},
       {id: 3, body: "This is a third test", timestamp: Date.now()}
-    ]
+    ];
+    this.selectedNote = this.notes[0];
+  },
+  computed: {
+    transformedNotes: function() {
+      return this.notes
+        .filter(function(note) {
+          return note.body.toLowerCase().indexOf(this.searchNoteText.toLowerCase()) !== -1;
+        }.bind(this))
+        .sort(function(a, b) {
+          return b.timestamp - a.timestamp;
+        });
+    }
+  },
+  filters: {
+    formatTitle: function(body) {
+      var maxLength = 20;
+      if (body.length > maxLength) {
+        return body.substring(0, maxLength - 3) + '...';
+      } else if (body.length === 0) {
+        return "New note";
+      } else {
+        return body;
+      }
+    },
+    formatTimestamp: function(timestamp) {
+      return new Date(timestamp).toUTCString();
+    }
+  },
+  methods: {
+    selectNote: function(note) {
+      this.selectedNote = note;
+    },
+    createNote: function() {
+      var newNote = {
+        id: Date.now(),
+        body: "",
+        timestamp: Date.now()
+      };
+      this.notes.push(newNote);
+      this.selectedNote = newNote;
+    },
+    deleteNote: function(note) {
+      var index = this.notes.indexOf(this.selectedNote);
+      if (index !== -1) {
+        this.notes.splice(index, 1);
+        if (this.transformedNotes.length > 0) {
+          this.selectedNote = this.transformedNotes[0];
+        } else {
+          this.selectedNote = {};
+        }
+      }
+    }
+  },
+  watch: {
+    notes: {
+      handler: function() {
+        this.selectedNote.timestamp = Date.now();
+      },
+      deep: true
+    },
+    searchNoteText: function() {
+      if (this.transformedNotes.length === 0) {
+        this.selectedNote = {};
+      } else if (this.transformedNotes.indexOf(this.selectedNote) === -1) {
+        this.selectedNote = this.transformedNotes[0];
+      }
+    }
   }
 });
